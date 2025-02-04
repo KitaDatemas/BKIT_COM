7ae064681d16d8c90d3fa5acce61d749dcbaf9d2
Ngo Duc Luan <luan.ngoduc@hcmut.edu.vn>
2025-02-04 21:15:41 +0700

    Add Message_processing_Layer and crc

---
+++ Core/Inc/Message_Processing_Layer.h
@@ -0,0 +1,13 @@
+/*
+ * Message_Processing_Layer.h
+ *
+ *  Created on: Feb 4, 2025
+ *      Author: ADMIN
+ */
+
+#ifndef INC_MESSAGE_PROCESSING_LAYER_H_
+#define INC_MESSAGE_PROCESSING_LAYER_H_
+
+
+
+#endif /* INC_MESSAGE_PROCESSING_LAYER_H_ */
---
+++ Core/Src/Message_Processing_Layer.c
@@ -0,0 +1,8 @@
+/*
+ * Message_Processing_Layer.c
+ *
+ *  Created on: Feb 4, 2025
+ *      Author: ADMIN
+ */
+
+
